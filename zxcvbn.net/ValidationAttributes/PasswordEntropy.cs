07fc7d49adce5fd1cb9fe56b296af5491745b52b
darcythomas <github@darcy.io>
2014-04-29 07:42:32 +1200

    Added validation helpers

---
+++ zxcvbn.net/ValidationAttributes/PasswordEntropy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace zxcvbn.net
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
+    class PasswordEntropy : ValidationAttribute
+    {
+        public int _entropy { get; private set; }
+        public PasswordEntropy(int entropy) : base("Password needs to be stronger") { _entropy = entropy; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var password = value as String;
+            if (password == null) return new ValidationResult("A password is required.");
+            if (Zxcvbn.Entropy(password) < _entropy)
+            {
+                return new ValidationResult("Password is not strong enough.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+}
---
+++ zxcvbn.net/ValidationAttributes/StrongPassword.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace zxcvbn.net
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
+    class StrongPassword : ValidationAttribute
+    {
+        public StrongPassword() : base("Password needs to be stronger") { }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var password = value as String;
+            if (password == null) return new ValidationResult("A password is required.");
+            if (Zxcvbn.Score(password) <= 3)
+            {
+                return new ValidationResult("Password is not strong enough.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+}
