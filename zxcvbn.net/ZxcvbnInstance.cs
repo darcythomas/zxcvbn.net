f69d1015600f769c232237384464aa1dcc1ed085
darcythomas <github@darcy.io>
2014-09-23 14:45:47 +1200

    Changed js engine to V8

--- zxcvbn.net/ZxcvbnInstance.cs
+++ zxcvbn.net/ZxcvbnInstance.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using JavaScriptEngineSwitcher.Core;
 using Jurassic;
 using Jurassic.Library;
 using zxcvbn.net.Properties;
@@ -14,12 +15,14 @@ namespace zxcvbn.net
 {
     public class ZxcvbnInstance
     {
-        public ScriptEngine Engine { get { return LazyLoadEngine.Value; } }
-        private static readonly Lazy<ScriptEngine> LazyLoadEngine = new Lazy<ScriptEngine>(ScriptEngine);
+        public IJsEngine Engine { get { return LazyLoadEngine.Value; } }
+        private static readonly Lazy<IJsEngine> LazyLoadEngine = new Lazy<IJsEngine>(ScriptEngine);
 
-        private static ScriptEngine ScriptEngine()
+        private static IJsEngine ScriptEngine()
         {
-            ScriptEngine engine = new ScriptEngine();
+            //ScriptEngine engine = new ScriptEngine();
+
+            IJsEngine engine = JsEngineSwitcher.Current.CreateJsEngineInstance("V8JsEngine");
 
             //The global object 'exports', is needed for the zxcvbn.js lib. Used for scoping.
             engine.Execute("exports = {};");
