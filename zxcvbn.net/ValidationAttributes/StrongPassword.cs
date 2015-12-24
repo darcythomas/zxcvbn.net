7f4077008fb3fc55098f8db53e9961c846d03266
Aaron Ulbricht <aaron.ulbricht@ucsf.edu>
2015-12-24 15:02:11 -0800

    Add Mvc client side validation.

--- zxcvbn.net/ValidationAttributes/StrongPassword.cs
+++ zxcvbn.net/ValidationAttributes/StrongPassword.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web.Mvc;
 using System.ComponentModel.DataAnnotations;
 
 namespace zxcvbn.net
 {
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
-    public class StrongPassword : ValidationAttribute
+    public class StrongPassword : ValidationAttribute, IClientValidatable
     {
         public StrongPassword() : base("Password needs to be stronger") { }
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            var password = value as String;
+            var password = value as string;
             if (password == null) return new ValidationResult("A password is required.");
             if (Zxcvbn.Score(password) < 3)
             {
@@ -22,6 +20,19 @@ namespace zxcvbn.net
             }
 
             return ValidationResult.Success;
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            string errorMessage = ErrorMessageString;
+
+            // The value we set here are needed by the jQuery adapter
+            ModelClientValidationRule StrongPasswordRule = new ModelClientValidationRule();
+            StrongPasswordRule.ErrorMessage = errorMessage;
+            StrongPasswordRule.ValidationType = "strongpassword"; // This is the name the jQuery adapter will use
+            StrongPasswordRule.ValidationParameters.Add("param", "strongpassword");
+
+            yield return StrongPasswordRule;
         }
     }
 
--- zxcvbn.net/ValidationAttributes/VeryStrongPassword.cs
+++ zxcvbn.net/ValidationAttributes/VeryStrongPassword.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web.Mvc;
 using System.ComponentModel.DataAnnotations;
 
 namespace zxcvbn.net
 {
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
-    public class VeryStrongPassword : ValidationAttribute
+    public class VeryStrongPassword : ValidationAttribute, IClientValidatable
     {
         public VeryStrongPassword() : base("Password needs to be stronger") { }
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            var password = value as String;
+            var password = value as string;
             if (password == null) return new ValidationResult("A password is required.");
             if (Zxcvbn.Score(password) < 4)
             {
@@ -22,6 +20,19 @@ namespace zxcvbn.net
             }
 
             return ValidationResult.Success;
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            string errorMessage = ErrorMessageString;
+
+            // The value we set here are needed by the jQuery adapter
+            ModelClientValidationRule VeryStrongPasswordRule = new ModelClientValidationRule();
+            VeryStrongPasswordRule.ErrorMessage = errorMessage;
+            VeryStrongPasswordRule.ValidationType = "VeryStrongPassword"; // This is the name the jQuery adapter will use
+            VeryStrongPasswordRule.ValidationParameters.Add("param", "VeryStrongPassword");
+
+            yield return VeryStrongPasswordRule;
         }
     }
 
