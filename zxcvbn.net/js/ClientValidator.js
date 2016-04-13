f304b231cadfe22589257acaf3c022c1a3247101
Aaron Ulbricht <aaron.ulbricht@ucsf.edu>
2016-04-12 21:04:54 -0700

    Add javascript for client-side validations.

---
+++ zxcvbn.net/js/ClientValidator.js
@@ -0,0 +1,37 @@
+﻿(function ($) {
+    var zxcvbnWarning = function (value) {
+        var value = $(value).val();
+        var zx = zxcvbn(value);
+        var msg = "Please use a stronger password.";
+        if (zx.feedback) {
+            msg += '<ul>';
+            if (zx.feedback.warning)
+                msg += '<li>' + zx.feedback.warning + '</li>';
+            zx.feedback.suggestions.forEach(function (s) {
+                msg += '<li>' + s + '</li>';
+            })
+            msg += '</ul>';
+        }
+        return msg;
+    }
+
+    $.validator.addMethod('strongpassword', function (value, element, params) {
+        if (zxcvbn(value).score < 3)
+            return false;
+        return true
+    }, zxcvbnWarning);
+
+    $.validator.addMethod('verystrongpassword', function (value, element, params) {
+        if (zxcvbn(value).score < 4)
+            return false;
+        return true
+    }, zxcvbnWarning);
+
+    $.validator.unobtrusive.adapters.add("strongpassword", [], function (options) {
+        options.rules["strongpassword"] = options.element;
+    });
+
+    $.validator.unobtrusive.adapters.add("verystrongpassword", [], function (options) {
+        options.rules["verystrongpassword"] = options.element;
+    });
+})(jQuery);
